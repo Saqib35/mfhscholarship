0789c0a99a283225a7e4db32862ab049f667e9c9
Msaqib78 <94630654+Saqib35@users.noreply.github.com>
2023-08-09 14:24:39 +0500

    saqib

--- resources/views/includes/cssLinks.blade.php
+++ resources/views/includes/cssLinks.blade.php
@@ -8,3 +8,13 @@
     <link rel="stylesheet" href="{{ asset('assets/css/plugins/aos.min.css')}}"/>
     <link rel="stylesheet" href="{{ asset('assets/css/plugins/nice-select.min.css')}}"/>
     <link rel="stylesheet" href="{{ asset('assets/css/style.css')}}" />
+    <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" defer></script>
+    <script>
+    window.OneSignal = window.OneSignal || [];
+    OneSignal.push(function() {
+        OneSignal.init({
+        appId: "524c15cf-c4d6-4482-9fd0-3d2e7a9133f1",
+        });
+    });
+    </script>
+    
