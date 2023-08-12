54600fa6acc5435a029771e5207d05088616d22a
Msaqib78 <94630654+Saqib35@users.noreply.github.com>
2023-08-12 19:57:20 +0500

    meta

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    
+    <meta name="google-site-verification" content="o8RVhN02Sz7PmPdBG1ciz3Gtem3QAp2wBgA-AS81MkQ" />
     @include('includes.cssLinks')
     @yield('css')
     
